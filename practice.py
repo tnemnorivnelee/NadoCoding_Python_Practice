e503d47e354b2f9960e747af1be305f57a84ea25
tnemnorivnelee <tnemnorivnelee@gmail.com>
2022-08-24 19:07:01 +0900

    quiz7

--- practice.py
+++ practice.py
@@ -1,72 +1,93 @@
-# def open_account():
-#     print("새로운 계좌가 생성되었습니다")
-
-# open_account()
-
-# def deposit(balance, money):
-#     print("입금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance + money))
-#     return balance + money
-
-# def withdraw(balance, money):
-#     if balance >= money:
-#         print("출금이 완료되었습니다. 잔액은 {0} 원입니다.".format(balance - money))
-#         return balance - money
-#     else:
-#         print("출금이 완료되지 않았습니다. 잔액은 {0} 원입니다.".format(balance))
-#         return balance
-
-# def withdraw_night(balance, money):
-#     commission = 100
-#     return commission, balance - money - commission
-
-# balance = 0
-# balance = deposit(balance, 1000)
-# # balance = withdraw(balance, 2000)
-# # balance = withdraw(balance, 500)
-# commission, balance = withdraw_night(balance, 500)
-# print("수수료는 {0} 원이며, 잔액은 {1} 원입니다.".format(commission, balance))
-
-# def profile(name, age, main_lang):
-#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
-
-# def profile(name, age=17, main_lang="파이썬"):
-#     print("이름 : {0}\t나이 : {1}\t주 사용 언어 : {2}".format(name, age, main_lang))
-
-# profile("유재석")
-# profile("김태호")
-
-# def profile(name, age, main_lang):
-#     print(name, age, main_lang)
-
-# profile(name="lee", main_lang="python", age=20)
-
-# def profile(name, age, lang1, lang2, lang3, lang4, lang5):
-#     print("name : {0}\tage : {1}\t".format(name, age), end=" ")
-#     print(lang1, lang2, lang3, lang4, lang5)
-
-# def profile(name, age, *language):
-#     print("name : {0}\tage : {1}\t".format(name, age), end=" ")
-#     for lang in language:
-#         print(lang, end=" ")
-#     print()
-
-# profile("lee", 20, "python", "java", "c", "c++", "c#", "js")
-# profile("kim:", 25, "kotiln", "swift")
-
-gun = 10
-
-def checkpoint(soldiers):
-    global gun
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {0}".format(gun))
-
-def checkpoint_ret(gun, soldiers):
-    gun = gun - soldiers
-    print("[함수 내] 남은 총 : {0}".format(gun))
-    return gun
+# # import sys
+# # print("Python", "Java", file=sys.stdout)
+# # print("Python", "Java", file=sys.stderr)
 
 
-print("전체 총 : {0}".format(gun))
-# checkpoint(2)
-gun = checkpoint_ret(gun, 2)
-print("남은 총 : {0}".format(gun))
+# scores = {"수학":0, "영어":50, "코딩":100}
+# for subject, score in scores.items():
+#     # print(subject, score)
+#     print(subject.ljust(8), str(score).rjust(4), sep=":")
+
+# for number in range(1, 21):
+#     print("대기번호: " + str(number).zfill(3))
+
+# answer = input("아무 값이나 입력하세요 :")
+# print(type(answer))
+# print("입력하신 값은 " + answer + "입니다.")
+
+
+# print("{0: >10}".format(500))
+# print("{0: >+10}".format(500))
+# print("{0: >+10}".format(-500))
+
+# print("{0:_<10}".format(500))
+
+# print("{0:,}".format(100000000000))
+# print("{0:+,}".format(100000000000))
+# print("{0:+,}".format(-100000000000))
+
+# print("{0:^<+30,}".format(100000000000))
+
+# print("{0:f}".format(5/3))
+# print("{0:.2f}".format(5/3))
+
+
+# score_file = open("score.txt", "w", encoding="utf8")
+# print("수학: 0", file=score_file)
+# print("영어 : 50", file=score_file)
+# score_file.close()
+
+# score_file = open("score.txt", "a", encoding="utf8")
+# score_file.write("과학 : 80")
+# score_file.write("\n코딩 : 100")
+# score.file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.read())
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# print(score_file.readline(), end="")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# while True:
+#     line = score_file.readline()
+#     if not line:
+#         break
+#     print(line, end="")
+# score_file.close()
+
+# score_file = open("score.txt", "r", encoding="utf8")
+# lines = score_file.readlines()
+# for line in lines:
+#     print(line, end="")
+# score_file.close()
+
+
+# import pickle
+# # profile_file = open("profile.pickle", "wb")
+# # profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
+# # print(profile)
+# # pickle.dump(profile, profile_file)
+# # profile_file.close()
+
+# profile_file = open("profile.pickle", "rb")
+# profile = pickle.load(profile_file)
+# print(profile)
+# profile_file.close()
+
+
+# import pickle
+
+# with open("profile.pickle", "rb") as profile_file:
+#     print(pickle.load(profile_file))
+
+# with open("study.txt", "w", encoding="utf8") as study_file:
+#     study_file.write("파이썬을 열심히 공부하고 있어요")
+
+with open("study.txt", "r", encoding="utf8") as study_file:
+    print(study_file.read())
