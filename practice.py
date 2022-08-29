08cd6db1cfeeef7a989212c6951198760d0a1d3e
tnemnorivnelee <tnemnorivnelee@gmail.com>
2022-08-29 15:18:38 +0900

    9-6 다중상속까지

--- practice.py
+++ practice.py
@@ -1,93 +1,43 @@
-# # import sys
-# # print("Python", "Java", file=sys.stdout)
-# # print("Python", "Java", file=sys.stderr)
+class Unit:
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+# 공격 유닛
+class AttackUnit(Unit):
+    def __init__(self, name, hp, damage):
+        Unit.__init__(self, name, hp)
+        self.damage = damage
+        print("{0} 유닛이 생성되었습니다.".format(self.name))
+        print("체력 {0}, 공격력 {1}".format(self.hp, self.damage))
+
+    def attack(self, location):
+        print("{0} : {1} 방향으로 적군을 공격합니다. 공격력 {2}".format(self.name, location, self.damage))
+
+    def damaged(self, damage):
+        print("{0} : {1} 데미지를 입었습니다.".format(self.name, damage))
+        self.hp -= damage
+        print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+        if self.hp <= 0:
+            print("{0} : 파괴되었습니다.".format(self.name))
+        
+# 드랍쉽 : 공중 유닛, 수송기. 마린 / 파이어뱃 / 탱크 등을 수송. 공격 X
+
+# 날 수 있는 기능을 가진 클래스
+class Flyable:
+    def __init__(self, flying_speed):
+        self.flying_speed = flying_speed
+
+    def fly(self, name, location):
+        print("{0} : {1} 방향으로 날아갑니다. [속도 {2}]".format(name, location, self.flying_speed))
+
+# 공중 공격 유닛 클래스
+class FlyableAttackUnit(AttackUnit, Flyable):
+    def __init__(self, name, hp, damage, flying_speed):
+        AttackUnit.__init__(self, name, hp, damage)
+        Flyable.__init__(self, flying_speed)
 
 
-# scores = {"수학":0, "영어":50, "코딩":100}
-# for subject, score in scores.items():
-#     # print(subject, score)
-#     print(subject.ljust(8), str(score).rjust(4), sep=":")
-
-# for number in range(1, 21):
-#     print("대기번호: " + str(number).zfill(3))
-
-# answer = input("아무 값이나 입력하세요 :")
-# print(type(answer))
-# print("입력하신 값은 " + answer + "입니다.")
-
-
-# print("{0: >10}".format(500))
-# print("{0: >+10}".format(500))
-# print("{0: >+10}".format(-500))
-
-# print("{0:_<10}".format(500))
-
-# print("{0:,}".format(100000000000))
-# print("{0:+,}".format(100000000000))
-# print("{0:+,}".format(-100000000000))
-
-# print("{0:^<+30,}".format(100000000000))
-
-# print("{0:f}".format(5/3))
-# print("{0:.2f}".format(5/3))
-
-
-# score_file = open("score.txt", "w", encoding="utf8")
-# print("수학: 0", file=score_file)
-# print("영어 : 50", file=score_file)
-# score_file.close()
-
-# score_file = open("score.txt", "a", encoding="utf8")
-# score_file.write("과학 : 80")
-# score_file.write("\n코딩 : 100")
-# score.file.close()
-
-# score_file = open("score.txt", "r", encoding="utf8")
-# print(score_file.read())
-# score_file.close()
-
-# score_file = open("score.txt", "r", encoding="utf8")
-# print(score_file.readline(), end="")
-# print(score_file.readline(), end="")
-# print(score_file.readline(), end="")
-# print(score_file.readline(), end="")
-# score_file.close()
-
-# score_file = open("score.txt", "r", encoding="utf8")
-# while True:
-#     line = score_file.readline()
-#     if not line:
-#         break
-#     print(line, end="")
-# score_file.close()
-
-# score_file = open("score.txt", "r", encoding="utf8")
-# lines = score_file.readlines()
-# for line in lines:
-#     print(line, end="")
-# score_file.close()
-
-
-# import pickle
-# # profile_file = open("profile.pickle", "wb")
-# # profile = {"이름":"박명수", "나이":30, "취미":["축구", "골프", "코딩"]}
-# # print(profile)
-# # pickle.dump(profile, profile_file)
-# # profile_file.close()
-
-# profile_file = open("profile.pickle", "rb")
-# profile = pickle.load(profile_file)
-# print(profile)
-# profile_file.close()
-
-
-# import pickle
-
-# with open("profile.pickle", "rb") as profile_file:
-#     print(pickle.load(profile_file))
-
-# with open("study.txt", "w", encoding="utf8") as study_file:
-#     study_file.write("파이썬을 열심히 공부하고 있어요")
-
-with open("study.txt", "r", encoding="utf8") as study_file:
-    print(study_file.read())
+# 발키리 : 공중 공격 유닛, 한번에 14발 미사일 발사.
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
